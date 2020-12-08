edc7923e39a544c324fb22c495bcafd784330bf1
Aleksandr <3pervui911@gmail.com>
2020-12-08 15:08:18 +0200

    first commit

---
+++ app/dist/sctipt.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $('.carousel').carousel();
+});
